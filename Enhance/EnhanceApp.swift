7496b422f37f36a09a5035a185f79f966cc9f112
Dory Little <dory.little@gmail.com>
2025-03-22 17:27:08 -0700

    Initial Commit

---
+++ Enhance/EnhanceApp.swift
@@ -0,0 +1,17 @@
+//
+//  EnhanceApp.swift
+//  Enhance
+//
+//  Created by Dorington Little on 3/22/25.
+//
+
+import SwiftUI
+
+@main
+struct EnhanceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ EnhanceTests/EnhanceTests.swift
@@ -0,0 +1,17 @@
+//
+//  EnhanceTests.swift
+//  EnhanceTests
+//
+//  Created by Dorington Little on 3/22/25.
+//
+
+import Testing
+@testable import Enhance
+
+struct EnhanceTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
